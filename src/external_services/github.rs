34d39ccc5636ecda8dd9860cb2828853b68c0260
esraa <jbesraa@gmail.com>
2023-08-16 17:33:01 +0300

    Production hot fix
    Currently production fails to fetch all apps -
    pushing a nasty solution for now to fix it and
    we will improve it as we go on.

--- src/external_services/github.rs
+++ src/external_services/github.rs
@@ -1,8 +1,10 @@
+#![allow(dead_code)]
 use http::header::USER_AGENT;
 use http::{Request, Uri};
 use hyper_rustls::HttpsConnectorBuilder;
+use markdown::{mdast::Node, to_mdast, ParseOptions};
 
-use crate::core::LDNPullRequest;
+use crate::core::{LDNPullRequest, ParsedApplicationDataFields};
 use octocrab::auth::AppAuth;
 use octocrab::models::issues::{Comment, Issue};
 use octocrab::models::pulls::PullRequest;
@@ -15,11 +17,26 @@ use octocrab::{AuthState, Error as OctocrabError, Octocrab, OctocrabBuilder, Pag
 use std::sync::Arc;
 
 const GITHUB_API_URL: &str = "https://api.github.com";
-const GITHUB_OWNER: &str = "filecoin-project";
-const GITHUB_REPO: &str = "filplus-tooling-backend-test";
-const APP_ID: u64 = 373258;
-const APP_INSTALLATION_ID: u64 = 40514592;
-const MAIN_BRANCH_HASH: &str = "650a0aec11dc1cc436a45b316db5bb747e518514";
+
+struct GithubParams<'a> {
+    pub owner: &'a str,
+    pub repo: &'a str,
+    pub app_id: u64,
+    pub installation_id: u64,
+    pub main_branch_hash: &'a str,
+}
+
+impl GithubParams<'static> {
+    fn test_env() -> Self {
+        Self {
+            owner: "filecoin-project",
+            repo: "filplus-tooling-backend-test",
+            app_id: 373258,
+            installation_id: 40514592,
+            main_branch_hash: "650a0aec11dc1cc436a45b316db5bb747e518514",
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct CreateMergeRequestData {
@@ -31,12 +48,22 @@ pub struct CreateMergeRequestData {
 }
 
 #[derive(Debug)]
-pub struct GithubWrapper {
+pub struct GithubWrapper<'a> {
     pub inner: Arc<Octocrab>,
+    pub owner: &'a str,
+    pub repo: &'a str,
+    pub main_branch_hash: &'a str,
 }
 
-impl GithubWrapper {
+impl GithubWrapper<'static> {
     pub fn new() -> Self {
+        let GithubParams {
+            owner,
+            repo,
+            app_id,
+            installation_id,
+            main_branch_hash,
+        } = GithubParams::test_env();
         let connector = HttpsConnectorBuilder::new()
             .with_native_roots() // enabled the `rustls-native-certs` feature in hyper-rustls
             .https_only()
@@ -57,16 +84,17 @@ impl GithubWrapper {
                 "octocrab".parse().unwrap(),
             )])))
             .with_auth(AuthState::App(AppAuth {
-                app_id: APP_ID.into(),
+                app_id: app_id.into(),
                 key,
             }))
             .build()
             .expect("Could not create Octocrab instance");
-        let iod: InstallationId = APP_INSTALLATION_ID
-            .try_into()
-            .expect("Invalid installation id");
+        let iod: InstallationId = installation_id.try_into().expect("Invalid installation id");
         let installation = octocrab.installation(iod);
         Self {
+            main_branch_hash,
+            owner,
+            repo,
             inner: Arc::new(installation),
         }
     }
@@ -74,7 +102,7 @@ impl GithubWrapper {
     pub async fn list_issues(&self) -> Result<Vec<Issue>, OctocrabError> {
         let iid = self
             .inner
-            .issues(GITHUB_OWNER, GITHUB_REPO)
+            .issues(self.owner, self.repo)
             .list()
             .state(State::Open)
             .send()
@@ -83,11 +111,7 @@ impl GithubWrapper {
     }
 
     pub async fn list_issue(&self, number: u64) -> Result<Issue, OctocrabError> {
-        let iid = self
-            .inner
-            .issues(GITHUB_OWNER, GITHUB_REPO)
-            .get(number)
-            .await?;
+        let iid = self.inner.issues(self.owner, self.repo).get(number).await?;
         Ok(iid)
     }
 
@@ -98,7 +122,7 @@ impl GithubWrapper {
     ) -> Result<Comment, OctocrabError> {
         let iid = self
             .inner
-            .issues(GITHUB_OWNER, GITHUB_REPO)
+            .issues(self.owner, self.repo)
             .create_comment(number, body)
             .await?;
         Ok(iid)
@@ -107,7 +131,7 @@ impl GithubWrapper {
     pub async fn list_pull_requests(&self) -> Result<Vec<PullRequest>, OctocrabError> {
         let iid = self
             .inner
-            .pulls(GITHUB_OWNER, GITHUB_REPO)
+            .pulls(self.owner, self.repo)
             .list()
             .state(State::Open)
             .send()
@@ -122,17 +146,30 @@ impl GithubWrapper {
     ) -> Result<octocrab::models::commits::Comment, OctocrabError> {
         let iid = self
             .inner
-            .commits(GITHUB_OWNER, GITHUB_REPO)
+            .commits(self.owner, self.repo)
             .create_comment(branch_name, commit_body)
             .send()
             .await?;
         Ok(iid)
     }
 
+    pub async fn get_pull_request_files(
+        &self,
+        number: u64,
+    ) -> Result<Vec<octocrab::models::pulls::FileDiff>, OctocrabError> {
+        let iid: Page<octocrab::models::pulls::FileDiff> = self
+            .inner
+            .pulls(self.owner, self.repo)
+            .media_type(octocrab::params::pulls::MediaType::Full)
+            .list_files(number)
+            .await?;
+        Ok(iid.items.into_iter().map(|i| i.into()).collect())
+    }
+
     pub async fn list_branches(&self) -> Result<Vec<Branch>, OctocrabError> {
         let iid = self
             .inner
-            .repos(GITHUB_OWNER, GITHUB_REPO)
+            .repos(self.owner, self.repo)
             .list_branches()
             .send()
             .await?;
@@ -166,11 +203,7 @@ impl GithubWrapper {
     }
 
     pub async fn list_pull_request(&self, number: u64) -> Result<PullRequest, OctocrabError> {
-        let iid = self
-            .inner
-            .pulls(GITHUB_OWNER, GITHUB_REPO)
-            .get(number)
-            .await?;
+        let iid = self.inner.pulls(self.owner, self.repo).get(number).await?;
         Ok(iid)
     }
 
@@ -182,7 +215,7 @@ impl GithubWrapper {
     ) -> Result<PullRequest, OctocrabError> {
         let iid = self
             .inner
-            .pulls(GITHUB_OWNER, GITHUB_REPO)
+            .pulls(self.owner, self.repo)
             .create(title, head, "main")
             .body(body)
             .maintainer_can_modify(true)
@@ -198,7 +231,7 @@ impl GithubWrapper {
     ) -> Result<PullRequest, OctocrabError> {
         let iid = self
             .inner
-            .pulls(GITHUB_OWNER, GITHUB_REPO)
+            .pulls(self.owner, self.repo)
             .update(number)
             .body(body)
             .send()
@@ -215,7 +248,7 @@ impl GithubWrapper {
     ) -> Result<FileUpdate, OctocrabError> {
         let iid = self
             .inner
-            .repos(GITHUB_OWNER, GITHUB_REPO)
+            .repos(self.owner, self.repo)
             .create_file(path, message, content)
             .branch(branch)
             .send()
@@ -227,11 +260,7 @@ impl GithubWrapper {
         &self,
         number: u64,
     ) -> Result<octocrab::models::pulls::PullRequest, OctocrabError> {
-        let iid = self
-            .inner
-            .pulls(GITHUB_OWNER, GITHUB_REPO)
-            .get(number)
-            .await?;
+        let iid = self.inner.pulls(self.owner, self.repo).get(number).await?;
         Ok(iid)
     }
 
@@ -242,7 +271,7 @@ impl GithubWrapper {
     ) -> Result<ContentItems, octocrab::Error> {
         let iid = self
             .inner
-            .repos(GITHUB_OWNER, GITHUB_REPO)
+            .repos(self.owner, self.repo)
             .get_content()
             .r#ref(branch)
             .path(path)
@@ -261,7 +290,7 @@ impl GithubWrapper {
     ) -> Result<FileUpdate, octocrab::Error> {
         let iid = self
             .inner
-            .repos(GITHUB_OWNER, GITHUB_REPO)
+            .repos(self.owner, self.repo)
             .update_file(path, message, content, file_sha)
             .branch(branch)
             .send()
@@ -269,43 +298,31 @@ impl GithubWrapper {
         Ok(iid)
     }
 
-    pub fn build_governance_review_branch(name: String) -> Result<Request<String>, http::Error> {
-        Ok(Request::builder()
-            .method("POST")
-            .uri(format!(
-                "https://api.github.com/repos/{}/{}/git/refs",
-                GITHUB_OWNER, GITHUB_REPO
-            ))
-            .body(format!(
-                r#"{{"ref": "refs/heads/{}/allocation","sha": "a9f99d9fc56cae689a0bf0ee177c266287eb48cd"}}"#,
-                name
-            ))?)
-    }
-
     pub fn build_remove_ref_request(&self, name: String) -> Result<Request<String>, http::Error> {
         let request = Request::builder()
             .method("DELETE")
             .uri(format!(
                 "https://api.github.com/repos/{}/{}/git/refs/heads/{}",
-                GITHUB_OWNER, GITHUB_REPO, name
+                self.owner, self.repo, name
             ))
             .body("".to_string())?;
         Ok(request)
     }
 
     pub fn build_create_ref_request(
+        &self,
         name: String,
         head_hash: Option<String>,
     ) -> Result<Request<String>, http::Error> {
         let hash = match head_hash {
             Some(hash) => hash,
-            None => MAIN_BRANCH_HASH.to_string(),
+            None => self.main_branch_hash.to_string(),
         };
         let request = Request::builder()
             .method("POST")
             .uri(format!(
                 "https://api.github.com/repos/{}/{}/git/refs",
-                GITHUB_OWNER, GITHUB_REPO
+                self.owner, self.repo
             ))
             .body(format!(
                 r#"{{"ref": "refs/heads/{}","sha": "{}" }}"#,
@@ -327,7 +344,7 @@ impl GithubWrapper {
     pub async fn create_issue(&self, title: &str, body: &str) -> Result<Issue, OctocrabError> {
         Ok(self
             .inner
-            .issues(GITHUB_OWNER, GITHUB_REPO)
+            .issues(self.owner, self.repo)
             .create(title)
             .body(body)
             .send()
@@ -337,11 +354,28 @@ impl GithubWrapper {
     pub async fn close_issue(&self, issue_number: u64) -> Result<Issue, OctocrabError> {
         Ok(self
             .inner
-            .issues(GITHUB_OWNER, GITHUB_REPO)
+            .issues(self.owner, self.repo)
             .update(issue_number)
             .state(IssueState::Closed)
             .send()
             .await?)
+    }
+
+    pub async fn get_all_pull_requests(&self) -> Result<Vec<u64>, OctocrabError> {
+        let mut pull_requests: Page<octocrab::models::pulls::PullRequest> = self
+            .inner
+            .pulls(self.owner, self.repo)
+            .list()
+            .state(State::Open)
+            .base("main")
+            .send()
+            .await?;
+        let pull_requests_vec: Vec<u64> = pull_requests
+            .take_items()
+            .into_iter()
+            .map(|pr| pr.number)
+            .collect();
+        Ok(pull_requests_vec)
     }
 
     pub async fn get_pull_request_by_head(
@@ -350,7 +384,7 @@ impl GithubWrapper {
     ) -> Result<Vec<PullRequest>, OctocrabError> {
         let mut pull_requests: Page<octocrab::models::pulls::PullRequest> = self
             .inner
-            .pulls(GITHUB_OWNER, GITHUB_REPO)
+            .pulls(self.owner, self.repo)
             .list()
             .state(State::Open)
             .head(head)
@@ -364,7 +398,7 @@ impl GithubWrapper {
     pub async fn close_pull_request(&self, number: u64) -> Result<PullRequest, OctocrabError> {
         Ok(self
             .inner
-            .pulls(GITHUB_OWNER, GITHUB_REPO)
+            .pulls(self.owner, self.repo)
             .update(number)
             .state(PullState::Closed)
             .send()
@@ -407,7 +441,7 @@ impl GithubWrapper {
     pub async fn merge_pull_request(&self, number: u64) -> Result<(), OctocrabError> {
         let _merge_res = self
             .inner
-            .pulls(GITHUB_OWNER, GITHUB_REPO)
+            .pulls(self.owner, self.repo)
             .merge(number)
             .send()
             .await?;
@@ -415,15 +449,145 @@ impl GithubWrapper {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct TestPatch {
+    pub hello: String,
+    pub second: String,
+    pub third: String,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+pub enum ParsedTestPatch {
+    Hello,
+    Second,
+    Third,
+}
+
+impl From<String> for ParsedTestPatch {
+    fn from(s: String) -> Self {
+        dbg!(&s);
+        match s.as_str() {
+            "Hello" => ParsedTestPatch::Hello,
+            "Second" => ParsedTestPatch::Second,
+            "Third" => ParsedTestPatch::Third,
+            _ => panic!("Unknown property"),
+        }
+    }
+}
+
+pub fn parser(body: &str) -> TestPatch {
+    let tree: Node = to_mdast(body, &ParseOptions::default()).unwrap();
+    let mut hello: Option<String> = None;
+    let mut second: Option<String> = None;
+    let mut third: Option<String> = None;
+    // let mut custom_notary: Option<String> = None;
+    for (index, i) in tree.children().unwrap().into_iter().enumerate().step_by(2) {
+        let prop: ParsedTestPatch = i.to_string().into();
+        let tree = tree.children().unwrap().into_iter();
+        let value = match tree.skip(index + 1).next() {
+            Some(v) => v.to_string(),
+            None => continue,
+        };
+        match prop {
+            ParsedTestPatch::Hello => {
+                hello = Some(value);
+            }
+            ParsedTestPatch::Second => {
+                second = Some(value);
+            }
+            ParsedTestPatch::Third => {
+                third = Some(value);
+            }
+        }
+    }
+    let parsed_ldn = TestPatch {
+        hello: hello.unwrap_or_else(|| "No Name".to_string()),
+        second: second.unwrap_or_else(|| "No Region".to_string()),
+        third: third.unwrap_or_else(|| "No Region".to_string()),
+    };
+    parsed_ldn
+}
+
+fn remove_invalid_chars(mut s: String) -> String {
+    s.retain(|x| !['+', '\n', '\'', '-'].contains(&x));
+    s
+}
+
+fn http_server() -> reqwest::Client {
+    let client = reqwest::Client::builder()
+        .user_agent("FP-CORE/0.1.0")
+        .connection_verbose(true)
+        .build()
+        .expect("Failed to build client");
+    client
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::external_services::github::GithubWrapper;
+    use crate::{
+        core::application::ApplicationFile,
+        external_services::github::{http_server, remove_invalid_chars, GithubWrapper, TestPatch},
+    };
 
     #[tokio::test]
     async fn test_basic_integration() {
         let gh = GithubWrapper::new();
-        assert!(gh.list_issues().await.is_ok());
-        assert!(gh.list_pull_requests().await.is_ok());
-        assert!(gh.list_branches().await.is_ok());
+        let mut pull_requests: Vec<u64> = gh.get_all_pull_requests().await.unwrap();
+        dbg!(&pull_requests);
+        let mut ret: Vec<ApplicationFile> = vec![];
+        while let Some(pr_number) = pull_requests.pop() {
+            let files = gh.get_pull_request_files(pr_number).await.unwrap();
+            let blolb_url = match files.get(0) {
+                Some(file) => file.raw_url.clone(),
+                None => continue,
+            };
+            let scheme = blolb_url.scheme();
+            let host = match blolb_url.host_str() {
+                Some(host) => host,
+                None => continue,
+            };
+            let path = blolb_url.path();
+            let url = format!("{}://{}{}", scheme, host, path);
+            let client = http_server();
+            let res = match client.get(&url).send().await {
+                Ok(res) => res,
+                Err(_) => {
+                    continue;
+                }
+            };
+            let res = match res.text().await {
+                Ok(res) => res,
+                Err(_) => {
+                    continue;
+                }
+            };
+            let res: ApplicationFile = match serde_json::from_str(&res) {
+                Ok(res) => res,
+                Err(_) => {
+                    continue;
+                }
+            };
+            ret.push(res);
+        }
+        dbg!(&ret);
+        // // dbg!(&files.get(0).unwrap().blob_url);
+        // dbg!(&files.get(0).unwrap().patch);
+        // reqwest.
+        // let url = format!(blolb_url.schema);
+        // let mut patch = files.get(0).unwrap().patch.clone().unwrap();
+        // let offset = patch.find("{").unwrap();
+        // let deleted = patch.drain(..offset).collect::<String>();
+        // // dbg!(&deleted);
+        // // dbg!(&patch);
+        // // dbg!(&patch);
+        // let parsed = remove_invalid_chars(patch);
+        // dbg!(&parsed);
+        // let test_patch: TestPatch = serde_json::from_str(&parsed).unwrap();
+        // dbg!(&test_patch);
+        // dbg!(&test_patch);
+        assert!(false);
+        // assert!(gh.list_issues().await.is_ok());
+        // assert!(gh.list_pull_requests().await.is_ok());
+        // assert!(gh.list_branches().await.is_ok());
     }
 }
