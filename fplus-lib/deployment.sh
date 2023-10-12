ee5b09998d187dfb8da662e957de7c6e40cdf7bf
kokal33 <vasilev.s@protonmail.com>
2023-10-12 15:13:41 +0200

    removed deployment.sh added to toml file

--- fplus-lib/deployment.sh
+++
@@ -1,8 +0,0 @@
-# you need to authenticate with AWS first
-# by running aws configure
-# and then run this script
-sudo docker build --platform=linux/amd64 . -t fp-core # build the image
-sudo docker tag fp-core:latest public.ecr.aws/b3c4u5n1/filecoin-core-api:latest # tag the image
-sudo aws ecr-public get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin public.ecr.aws # login to the registry
-sudo docker push public.ecr.aws/b3c4u5n1/filecoin-core-api:latest # push the image to the registry
-
