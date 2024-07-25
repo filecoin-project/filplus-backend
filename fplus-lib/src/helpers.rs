2e10a7ba0d5620bd5e0910f192c75e5c227bc7d7
Kacper Żuk <kacper.zuk@neti-soft.com>
2024-07-25 15:08:35 +0200

    Support fractions in data amounts

--- fplus-lib/src/helpers.rs
+++ fplus-lib/src/helpers.rs
@@ -1,28 +1,9 @@
-use regex::Regex;
+use size::Size;
 
 pub fn parse_size_to_bytes(size: &str) -> Option<u64> {
-    let re = Regex::new(r"^(\d+)\s?([a-zA-Z]+)$").unwrap();
-    let caps = re.captures(size.trim())?;
-
-    let number = caps.get(1)?.as_str().parse::<u64>().ok()?;
-    let unit = caps.get(2)?.as_str().to_uppercase();
-
-    // Normalize the unit by removing any trailing 'i', 's' and converting to upper case
-    let normalized_unit = unit.trim_end_matches('S');
-
-    match normalized_unit {
-        "KIB" => Some(number * 1024),                             // 2^10
-        "MIB" => Some(number * 1024 * 1024),                      // 2^20
-        "GIB" => Some(number * 1024 * 1024 * 1024),               // 2^30
-        "TIB" => Some(number * 1024 * 1024 * 1024 * 1024),        // 2^40
-        "PIB" => Some(number * 1024 * 1024 * 1024 * 1024 * 1024), // 2^50
-        "KB" => Some(number * 1000),                              // 10^3
-        "MB" => Some(number * 1000 * 1000),                       // 10^6
-        "GB" => Some(number * 1000 * 1000 * 1000),                // 10^9
-        "TB" => Some(number * 1000 * 1000 * 1000 * 1000),         // 10^12
-        "PB" => Some(number * 1000 * 1000 * 1000 * 1000 * 1000),  // 10^15
-        _ => None,                                                // Unsupported unit
-    }
+    let size = Size::from_str(size).ok()?;
+    let bytes = size.bytes();
+    bytes.try_into().ok()
 }
 
 pub fn compare_allowance_and_allocation(
@@ -82,5 +63,17 @@ mod tests {
     fn parse_size_to_bytes_should_work_without_whitespace() {
         let res = parse_size_to_bytes("2PiB");
         assert_eq!(res, Some(2251799813685248));
+    }
+
+    #[test]
+    fn should_work_with_fractions() {
+        let res = parse_size_to_bytes("4.32PiB");
+        assert_eq!(res, Some(4863887597560136));
+    }
+
+    #[test]
+    fn should_work_with_fractions_with_space() {
+        let res = parse_size_to_bytes("4.32 PiB");
+        assert_eq!(res, Some(4863887597560136));
     }
 }
