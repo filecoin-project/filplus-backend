dc813dc0d26d2f5d26599c870127b1f86e759a26
Filip Lelek <filip.lelek@neti-soft.com>
2024-08-20 17:02:14 +0200

    Migrate from interplanetary to datacapstats api

--- fplus-lib/src/config.rs
+++ fplus-lib/src/config.rs
@@ -23,10 +23,7 @@ pub fn default_env_vars() -> &'static HashMap<&'static str, &'static str> {
             "https://fp-core.dp04sa0tdc6pk.us-east-1.cs.amazonlightsail.com",
         );
         m.insert("FILPLUS_ENV", "staging");
-        m.insert(
-            "GLIF_NODE_URL",
-            "http://electric-publicly-adder.ngrok-free.app/rpc/v0",
-        );
+        m.insert("GLIF_NODE_URL", "https://api.node.glif.io/rpc/v1");
         m.insert("ISSUE_TEMPLATE_VERSION", "1.3");
         m.insert(
             "GITCOIN_PASSPORT_DECODER",
@@ -36,10 +33,7 @@ pub fn default_env_vars() -> &'static HashMap<&'static str, &'static str> {
         m.insert("GITCOIN_MINIMUM_SCORE", "30");
         m.insert("KYC_URL", "https://kyc.allocator.tech");
         m.insert("RPC_URL", "https://mainnet.optimism.io");
-        m.insert(
-            "DMOB_API_URL",
-            "https://api.filplus.d.interplanetary.one/public/api",
-        );
+        m.insert("DMOB_API_URL", "https://api.datacapstats.io/public/api");
         m.insert("DMOB_API_KEY", "5c993a17-7b18-4ead-a8a8-89dad981d87e");
         m
     })
