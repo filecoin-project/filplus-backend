24944a7ffd520f34e368f5e26e13ecb0a94a6d57
Filip-L <lelekfilip@gmail.com>
2025-02-04 11:05:23 +0100

    Fix migration

--- manual-migrations/2025-01-31.sql
+++ manual-migrations/2025-01-31.sql
@@ -1,9 +1,6 @@
-CREATE TABLE public.comparable_applications
+CREATE TABLE comparable_applications
 (
     client_address text NOT NULL,
     application jsonb NOT NULL,
     PRIMARY KEY (client_address)
 );
-
-ALTER TABLE IF EXISTS public.comparable_applications
-    OWNER to postgres;
