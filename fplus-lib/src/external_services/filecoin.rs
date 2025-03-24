f3516d4a56cde511742bcbd48f024e22a47a569a
Filip-L <67787091+Filip-L@users.noreply.github.com>
2025-03-24 13:36:42 +0100

    [FIL-647] Check if allowance is sufficient for autoallocator (#261)
    * Check if allowance is sufficient for autoallocator

--- fplus-lib/src/external_services/filecoin.rs
+++ fplus-lib/src/external_services/filecoin.rs
@@ -107,3 +107,24 @@ pub async fn filecoin_address_to_evm_address(address: &str) -> Result<String, re
         .await?;
     Ok(response.result)
 }
+
+pub async fn evm_address_to_filecoin_address(address: &str) -> Result<String, reqwest::Error> {
+    let node_url = get_env_var_or_default("GLIF_NODE_URL");
+
+    let client = reqwest::Client::new();
+    let body = json!({
+        "jsonrpc": "2.0",
+        "method": "Filecoin.EthAddressToFilecoinAddress",
+        "params": [address],
+        "id": 0
+    });
+
+    let request = client.post(&node_url).json(&body);
+
+    let response = request
+        .send()
+        .await?
+        .json::<StateVerifiedClientStatusResponse>()
+        .await?;
+    Ok(response.result)
+}
